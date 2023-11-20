a6cf9b8ff939dd412c7b87405c5eddc2b3a4dc45
Abhijith Ganesh <git.watch.abhijith@outlook.com>
2023-11-21 01:57:51 +0530

    addition of editors
    Signed-off-by: Abhijith Ganesh <git.watch.abhijith@outlook.com>

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,8 +1,11 @@
 'use client'
-import { TiDocumentText } from "react-icons/ti";
+import { Button } from "@radix-ui/themes";
+import { useEffect, useRef } from "react";
 import { FaPlus } from "react-icons/fa6";
 import { IoSettingsOutline } from "react-icons/io5";
-import { Button } from "@radix-ui/themes";
+import { TiDocumentText } from "react-icons/ti";
+
+import EditorJS from "@editorjs/editorjs";
 
 interface SidebarButtonProps {
     text: string;
@@ -19,6 +22,45 @@ const SidebarButton = (props: SidebarButtonProps) => {
 }
 
 export default function Page() {
+    const ejInstance = useRef<EditorJS>();
+
+    const DEFAULT_INITIAL_DATA = {
+        "time": new Date().getTime(),
+        "blocks": [
+            {
+                "type": "header",
+                "data": {
+                    "text": "This is my awesome editor!",
+                    "level": 1
+                }
+            },
+        ]
+    }
+    useEffect(() => {
+        if (ejInstance.current === null) {
+            initEditor();
+        }
+
+        return () => {
+            ejInstance?.current?.destroy();
+        };
+    }, []);
+    const initEditor = () => {
+        const editor = new EditorJS({
+            holder: 'editorjs',
+            onReady: () => {
+                ejInstance.current = editor;
+            },
+            autofocus: true,
+            data: DEFAULT_INITIAL_DATA,
+            onChange: async () => {
+                let content = await editor.saver.save();
+
+                console.log(content);
+            }
+        });
+    };
+
     return (
         <>
             <section className="grid lg:grid-cols-10 sm:grid-cols-1 md:grid-cols-1 px-28 min-h-screen gap-6">
@@ -35,10 +77,11 @@ export default function Page() {
                             <section className="text-4xl font-extrabold tracking-tight text-slate-50">Your posts</section>
                             <section className="py-2 text-lg font-medium text-slate-400 tracking-tight">Create, Edit and Manage posts</section>
                         </section>
-                        <Button radius="medium" className="p-2 rounded-md bg-slate-50 text-black flex gap-2 items-center" onClick={() => { }}>
-                            <FaPlus className="text-black text-sm" /><section className="text-lg font-medium"> Create new post</section>
+                        <Button radius="medium" className="p-2 rounded-md bg-slate-50 hover:bg-slate-100 text-black flex gap-2 items-center" onClick={() => { }}>
+                            <FaPlus className="text-black text-sm" /><section className="text-md font-medium"> Create new post</section>
                         </Button>
                     </section>
+                    <section id="editorjs" className="w-full h-full bg-slate-900 rounded-md"></section>
                 </section>
             </section>
         </>
